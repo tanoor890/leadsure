b62fbfdd10ca9990b5c1430ff653136158094e08
tanoor890 <tanoor2305101802@diu.edu.bd>
2025-08-02 23:42:11 +0600

    upadted vite.ts in client

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,12 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
-
 export default defineConfig({
-  plugins: [
-    react(),
-    runtimeErrorOverlay()
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(import.meta.dirname, "client", "src"),
