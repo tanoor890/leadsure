fdbd2d0050d62619cd9442ada29b8745bab38b81
tanoor890 <tanoor2305101802@diu.edu.bd>
2025-08-02 23:26:19 +0600

    Clean Vite config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,15 +6,7 @@ import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 export default defineConfig({
   plugins: [
     react(),
-    runtimeErrorOverlay(),
-    ...(process.env.NODE_ENV !== "production" &&
-    process.env.REPL_ID !== undefined
-      ? [
-          await import("@replit/vite-plugin-cartographer").then((m) =>
-            m.cartographer(),
-          ),
-        ]
-      : []),
+    runtimeErrorOverlay()
   ],
   resolve: {
     alias: {
